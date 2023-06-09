3a13e87bed4f5878d559cf48c2b8d5117830be35
luis <luis@gmail.com>
2023-06-08 19:43:19 -0500

    taller 14: vue.js v-on

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@ console.log('Componente Vue');
 console.log(Vue);
 
 const app = Vue.createApp({
-    template: `
-    <h1> Hola Mundo</h1>
-    <p> Desde Vue.js</p>
-    `
+    //template: `
+    //<h1> Hola Mundo</h1>
+    //<p> Desde Vue.js</p>
+    //<p>{{1+1}}</p>
+    //`
+    data() {
+        return {
+            nombre: 'Edison',
+            apellido: 'Cayambe',
+            mensaje: 'Hola mundo 2 desde Vue.js'
+        }
+    },
+    methods: {
+        cambiarNombre() {
+            console.log('Boton cambiar Nombre');
+            this.nombre = 'LUIS'
+        },
+        cambiarApellido(){
+            console.log('Boton cambiar Apellido');
+            this.apellido = 'ORTIZ'
+        }
+    }
 })
 
 app.mount('#app')
---
+++ js/vFor.js
@@ -0,0 +1,12 @@
+
+
+const app = Vue.createApp({
+    //template: `
+    //<h1> Hola Mundo</h1>
+    //<p> Desde Vue.js</p>
+    //<p>{{1+1}}</p>
+    //`
+
+})
+
+app.mount('#app2')
