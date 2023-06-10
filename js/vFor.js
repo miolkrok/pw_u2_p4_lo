aff9f6b6d1f3719c5d88a1f5f9523de0a67c40af
luis <luis@gmail.com>
2023-06-10 10:47:44 -0500

    Taller 15: v-For y v-model

--- js/vFor.js
+++ js/vFor.js
@@ -1,4 +1,15 @@
-
+const arreglo = [{ nombre: "Luis", apellido: "Ortiz" },
+{ nombre: "Ariel", apellido: "Ortega" },
+{ nombre: "Francisco", apellido: "Orellana" },
+{ nombre: "Carlos", apellido: "Montalvo" },
+{ nombre: "Paulo", apellido: "Aldaz" },
+{ nombre: "Pepe", apellido: "Benalcazar" },
+{ nombre: "Juan", apellido: "Jimenez" },
+{ nombre: "Nicolas", apellido: "Carrera" },
+{ nombre: "Gabriel", apellido: "Jesus" },
+{ nombre: "Vinicius", apellido: "Jr" }
+]
+console.log(arreglo);
 
 const app = Vue.createApp({
     //template: `
@@ -6,6 +17,39 @@ const app = Vue.createApp({
     //<p> Desde Vue.js</p>
     //<p>{{1+1}}</p>
     //`
+    data() {
+        return {
+            miarreglo: arreglo,
+            nombre: 'LUIS'
+        }
+    },
+    methods: {
+        agregarEstudiante(event){
+            console.log('Vamos agregar')
+            console.log(this.nombre)
+            console.log(event)
+            console.log(event.charCode)
+            if(event.charCode=='13'){
+                console.log('Presiono enter')
+                const nuevoEstudiante = {
+                    nombre: this.nombre,
+                    apellido: this.apellido
+                }
+                //this.miarreglo.unshift(nuevoEstudiante)
+                this.miarreglo.push(nuevoEstudiante)
+
+            } else {
+                console.log('No es enter')
+            }
+        },agregarEstudiante2(){
+            console.log('Vamos agregar')
+            const nuevoEstudiante = {
+                nombre: this.nombre,
+                apellido: this.apellido
+            }
+            this.miarreglo.unshift(nuevoEstudiante)
+        }
+    }
 
 })
 
